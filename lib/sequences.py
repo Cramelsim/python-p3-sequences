b1f36e8b3f7279e361aa0b88042dc2e059a788d0
MELODY SIMWA <simwamelody@gmail.com>
2025-02-12 13:43:22 +0300

    update sequences.py

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])  # Print an empty list when n is 0 or negative
+        return
+    
+    fibonacci_sequence = [0, 1]
+    
+    for _ in range(2, length):
+        fibonacci_sequence.append(fibonacci_sequence[-1] + fibonacci_sequence[-2])
+    
+    print(fibonacci_sequence[:length])
